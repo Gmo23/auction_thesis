5dbf3dfdfe8860fc73aaf2fba070ea5afe1e47d0
George Morris <georgemorris458@gmail.com>
2025-09-03 09:08:30 +0100

    Refined analysis of asymmetric values

--- mutiple_simulations_results.py
+++ mutiple_simulations_results.py
@@ -5,14 +5,14 @@ import random
 NUM_SIMULATIONS = 100
 MAX_ROUNDS = 1_000_000
 CONVERGENCE_LIMIT = 1000
-AUCTION_CLASS = FPA_AuctionEnvironment  # or SPA_AuctionEnvironment
+AUCTION_CLASS = FPA_AuctionEnvironment  # FPA_Auction_Environment or SPA_AuctionEnvironment
 
 summary_results = []
 
 for sim_id in range(NUM_SIMULATIONS):
     bidders = [
-        EpsilonGreedy(name="Agent1", value=1, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101),
-        EpsilonGreedy(name="Agent2", value=1, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101)
+        EpsilonGreedy(name="Agent1", value=1, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101, stochastic=0),
+        EpsilonGreedy(name="Agent2", value=0.95, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101, stochastic=0)
     ]
     env = AUCTION_CLASS(bidders)
 
@@ -57,4 +57,4 @@ for sim_id in range(NUM_SIMULATIONS):
 # Save to file
 import pandas as pd
 df = pd.DataFrame(summary_results)
-df.to_csv("FPA_value_0_4_results.csv", index=False)
+df.to_csv("FPA_value_0_95_results.csv", index=False)
--- results.py
+++ results.py
@@ -17,11 +17,11 @@ from framework import (
 """
 
 #Define the bidder objects
-bidder1 = EpsilonGreedy(name="Agent1", value=1, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101)
-bidder2 = EpsilonGreedy(name="Agent2", value=1, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101)
+bidder1 = EpsilonGreedy(name="Agent1", value=1, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101, stochastic=0)
+bidder2 = EpsilonGreedy(name="Agent2", value=1, a=0.025, b=0.0002, alpha = 0.05, gamma = 0.99, init_param=101, stochastic=0.6)
 
 #run simulation for FPA or SPA
-simulation = AuctionSimulation(SPA_AuctionEnvironment, bidders=[bidder1, bidder2], max_rounds=1000000, convergence_limit=1000) # No. rounds of 1,000,000 is okay time wise
+simulation = AuctionSimulation(FPA_AuctionEnvironment, bidders=[bidder1, bidder2], max_rounds=1000000, convergence_limit=1000) # No. rounds of 1,000,000 is okay time wise
 
 #history is a list of tuples giving info for each round (bids, winner, winning_bid, reward)
 history = simulation.run()
